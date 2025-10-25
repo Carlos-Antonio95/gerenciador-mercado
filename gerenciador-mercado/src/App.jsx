239eebe8ba99444a08355d04573ffcb9439727fe
Carlos Antonio <carlosantonioob@gmail.com>
2025-10-25 19:39:09 -0300

    criando menu lateral esquerdo

--- gerenciador-mercado/src/App.jsx
+++ gerenciador-mercado/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//componente pai
 import './App.css'
-
+import './index.css'
+import FunctionalComponent from './components/FunctionalComponent.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <>
+  <div className="layout">
+    <aside className="menu-lateral">
+      <h2>Menu</h2>
+      <nav>
+        <ul><a href="#">Home</a></ul>
+        <ul><a href="#">Produtos</a></ul>
+        <ul><a href="#">Contato</a></ul>
+      </nav>
+    </aside>
+
+    <main className="conteudo">
+      <h1>Hello world React</h1>
+    </main>
+  </div>
+</>
+
     </>
   )
 }
